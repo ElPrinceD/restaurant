525cfeea82814c4995f5e1a09ac9b7e979082cfb
Elprinced <elprince.elp@gmail.com>
2024-10-23 10:12:48 +0100

    Updates

--- src/app/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mealReducer from "./mealSlice";
-
-export const store = configureStore({
-  reducer: { meals: mealReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
